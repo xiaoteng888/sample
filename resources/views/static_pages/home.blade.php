77d0461d459a88f4c81e0820e2c2a374ce2d01d6
vagrant <vagrant@homestead>
2020-05-25 11:49:21 +0000

    优化页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
-@section('title','主页')
+
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+	<h1>主页</h1>
+	<p class="lead">
+		laravel 入门项目
+	</p>
+	<p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+    </p>
+</div>
+
 @stop
